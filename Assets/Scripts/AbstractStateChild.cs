36207ea56580c0a615c96fe14f0141259cd0f944
TomoNanri <131641585+TomoNanri@users.noreply.github.com>
2023-09-11 17:36:17 +0900

    [update] クリーンアップ

--- Assets/Scripts/AbstractStateChild.cs
+++ Assets/Scripts/AbstractStateChild.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public abstract class AbstractStateChild : MonoBehaviour
 {
     protected AbstractStateController controller;
